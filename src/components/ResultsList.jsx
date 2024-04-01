1039ffc8bb9bd88cb5be7a240ce3a34bf1b628ae
Sandy Duncan <sandy.duncan.digital@gmail.com>
2024-04-01 22:28:48 +1030

    resultsList styles

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
+import "../styles/ResultsList.css"
 
 export default function ResultsList({ results, fetchMovieDetails, sliderValues, typeValue }) {
     const [filteredResults, setFilteredResults] = useState(null);
+    const [activeResult, setActiveResult] = useState();
 
+    // filter search results
     useEffect(() => {
         if (!results || !results.Search) return;
 
@@ -13,29 +16,44 @@ export default function ResultsList({ results, fetchMovieDetails, sliderValues, 
             }
             return (year >= sliderValues[0] && year <= sliderValues[1]) && Type === typeValue;
         });
+        
         console.log('filtered results', filtered)
         setFilteredResults(filtered);
+
     }, [results, sliderValues, typeValue]);
 
-    if (!results) {
-        return <div>No results found.</div>;
-    }
-
-    if (filteredResults === null) {
-        return <div>Loading...</div>;
+    if (!results || filteredResults === null) {
+        return ;
     }
 
     return (
-        <div>
-            <h3>Search Results ({filteredResults.length})</h3>
+        <div className="results-list">
+            <div className="results-list__total">{filteredResults.length} RESULTS</div>
             {filteredResults.map(({ Title, Year, imdbID, Poster }) => (
-                <div key={imdbID}>
-                    <p>{Title}</p>
-                    <p>Year: {Year}</p>
-                    {/* <img src={Poster} alt={Title} /> */}
-                    <button onClick={() => fetchMovieDetails(imdbID)}>View Details</button>
+                <div
+                    key={imdbID}
+                    className={`results-list__result ${activeResult === imdbID ? 'active' : ''}`}
+                >
+                    <div className="result__image">
+                        <img src={Poster} alt="" />
+                    </div>
+                    <div className="result__details">
+                        <a
+                            href="#"
+                            className="details__link stretched-link"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                fetchMovieDetails(imdbID);
+                                setActiveResult(imdbID);
+                            }}
+                        >
+                            {Title}
+                        </a>
+                        <span>{Year}</span>
+                    </div>
                 </div>
             ))}
         </div>
     );
 }
+
